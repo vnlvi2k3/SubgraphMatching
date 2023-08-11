51d8cba2d5a18c82a85df67e5fb5acca2ee6312f
vnlvi2k3 <139733764+vnlvi2k3@users.noreply.github.com>
2023-08-11 12:49:00 +0700

    Add files via upload

---
+++ new_dataset.py
@@ -0,0 +1,182 @@
+import os
+import pickle
+import random
+
+import networkx as nx
+import numpy as np
+import torch
+import utils
+from scipy.spatial import distance_matrix
+from torch.utils.data import Dataset
+from torch.utils.data.sampler import Sampler
+
+import dgl
+
+random.seed(42)
+
+
+def onehot_encoding_node(m, embedding_dim):
+    H = []
+    for i in m.nodes:
+        H.append(utils.node_feature(m, i, embedding_dim))
+    H = np.array(H)
+    return H
+
+
+class BaseDataset(Dataset):
+    def __init__(self, keys, data_dir, embedding_dim=20):
+        self.keys = keys
+        self.data_dir = data_dir
+        self.embedding_dim = embedding_dim
+
+    def __len__(self):
+        return len(self.keys)
+
+    def __getitem__(self, idx):
+        # idx = 0
+        key = self.keys[idx]
+        with open(os.path.join(self.data_dir, key), "rb") as f:
+            data = pickle.load(f)
+            if len(data) == 3:
+                m1, m2, mapping = data
+            else:
+                m1, m2 = data
+                mapping = []
+
+        # Prepare subgraph
+        n1 = m1.number_of_nodes()
+        adj1 = nx.to_numpy_array(m1) + np.eye(n1)
+        H1_pre = onehot_encoding_node(m1, self.embedding_dim)
+        sub_coordinates = []
+        for id in m1.nodes:
+            sub_coordinates.append(m1.nodes[id]["coords"])
+        sub_coordinates = np.vstack(sub_coordinates)
+
+        # Prepare source graph
+        n2 = m2.number_of_nodes()
+        adj2 = nx.to_numpy_array(m2) + np.eye(n2)
+        H2_pre = onehot_encoding_node(m2, self.embedding_dim)
+        graph_coordinates = []
+        for id in m2.nodes:
+            graph_coordinates.append(m2.nodes[id]["coords"])
+        graph_coordinates = np.vstack(graph_coordinates)
+
+        # Aggregation node encoding
+        agg_adj1 = np.zeros((n1 + n2, n1 + n2))
+        agg_adj1[:n1, :n1] = adj1
+        agg_adj1[n1:, n1:] = adj2
+        agg_adj2 = np.copy(agg_adj1)
+        dm = distance_matrix(H1, H2)
+        dm_new = np.zeros_like(dm)
+        dm_new[dm == 0.0] = 1.0
+        agg_adj2[:n1, n1:] = np.copy(dm_new)
+        agg_adj2[n1:, :n1] = np.copy(np.transpose(dm_new))
+
+        H1 = np.concatenate([H1_pre, np.zeros((n1, self.embedding_dim))], 1)
+        H2 = np.concatenate([np.zeros((n2, self.embedding_dim)), H2_pre], 1)
+        H = np.concatenate([H1, H2], 0)
+
+        # node indice for aggregation
+        valid = np.zeros((n1 + n2,))
+        valid[:n1] = 1
+
+        # create mapping matrix
+        mapping_matrix = np.zeros_like(agg_adj1)
+        if len(mapping) > 0:
+            mapping = np.array(mapping).T
+            mapping[1] = mapping[1] + n1
+            mapping_matrix[mapping[0], mapping[1]] = 1.0
+            mapping_matrix[mapping[1], mapping[0]] = 1.0
+
+        same_label_matrix = np.zeros_like(agg_adj1)
+        same_label_matrix[:n1, n1:] = np.copy(dm_new)
+        same_label_matrix[n1:, :n1] = np.copy(np.transpose(dm_new))
+
+        # iso to class
+        Y = 1 if "iso" in key else 0
+
+        # if n1+n2 > 300 : return None
+
+        lig_graph = dgl.from_networkx(m1, node_attrs=['label', 'coords'])
+        rec_graph = dgl.from_networkx(m2, node_attrs=['label', 'coords'])
+        lig_graph.ndata['feat'] = torch.from_numpy(H1_pre).float()
+        rec_graph.ndata['feat'] = torch.from_numpy(H2_pre).float()
+
+        sample = {
+            "H": H,
+            "A1": agg_adj1,
+            "A2": agg_adj2,
+            "Y": Y,
+            "V": valid,
+            "key": key,
+            "mapping": mapping_matrix,
+            "same_label": same_label_matrix,
+            "lig_graph": lig_graph,
+            "rec_graph": rec_graph,
+        }
+
+        return sample
+
+
+class UnderSampler(Sampler):
+    def __init__(self, weights, num_samples, replacement=True):
+        weights = np.array(weights) / np.sum(weights)
+        self.weights = weights
+        self.num_samples = num_samples
+        self.replacement = replacement
+
+    def __iter__(self):
+        # return iter(torch.multinomial(self.weights, self.num_samples, self.replacement).tolist())
+        retval = np.random.choice(
+            len(self.weights),
+            self.num_samples,
+            replace=self.replacement,
+            p=self.weights,
+        )
+        return iter(retval.tolist())
+
+    def __len__(self):
+        return self.num_samples
+
+
+def collate_fn(batch):
+    max_natoms = max([len(item["H"]) for item in batch if item is not None])
+
+    H = np.zeros((len(batch), max_natoms, batch[0]["H"].shape[-1]))
+    A1 = np.zeros((len(batch), max_natoms, max_natoms))
+    A2 = np.zeros((len(batch), max_natoms, max_natoms))
+    M = np.zeros((len(batch), max_natoms, max_natoms))
+    S = np.zeros((len(batch), max_natoms, max_natoms))
+    Y = np.zeros((len(batch),))
+    V = np.zeros((len(batch), max_natoms))
+    P = np.zeros((len(batch), max_natoms , 3))
+    Q = np.zeros((len(batch), max_natoms , 3))
+    lig_list = []
+    rec_list = []
+
+    for i in range(len(batch)):
+        natom = len(batch[i]["H"])
+
+        H[i, :natom] = batch[i]["H"]
+        A1[i, :natom, :natom] = batch[i]["A1"]
+        A2[i, :natom, :natom] = batch[i]["A2"]
+        M[i, :natom, :natom] = batch[i]["mapping"]
+        S[i, :natom, :natom] = batch[i]["same_label"]
+        Y[i] = batch[i]["Y"]
+        V[i, :natom] = batch[i]["V"]
+        lig_list.append(batch[i]["lig_graph"])
+        rec_list.append(batch[i]["rec_graph"])
+
+    
+    lig_graphs = dgl.batch(lig_list)
+    rec_graphs = dgl.batch(rec_list)
+    H = torch.from_numpy(H).float()
+    A1 = torch.from_numpy(A1).float()
+    A2 = torch.from_numpy(A2).float()
+    M = torch.from_numpy(M).float()
+    S = torch.from_numpy(S).float()
+    Y = torch.from_numpy(Y).float()
+    V = torch.from_numpy(V).float()
+
+    return H, A1, A2, M, S, Y, V, lig_graphs, rec_graphs
+
