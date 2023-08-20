29487ec43b6bc761a7132dcb363e454a9149a668
vnlvi2k3 <139733764+vnlvi2k3@users.noreply.github.com>
2023-08-20 11:34:35 +0700

    ppc_dataset.py

--- ppc_dataset.py
+++ ppc_dataset.py
@@ -6,10 +6,10 @@ import networkx as nx
 import numpy as np
 import torch
 import utils
+import dgl
 from scipy.spatial import distance_matrix
 from torch.utils.data import Dataset
 from torch.utils.data.sampler import Sampler
-import dgl
 
 random.seed(42)
 
@@ -46,19 +46,11 @@ class BaseDataset(Dataset):
         n1 = m1.number_of_nodes()
         adj1 = nx.to_numpy_array(m1) + np.eye(n1)
         H1 = onehot_encoding_node(m1, self.embedding_dim)
-        sub_coordinates = []
-        for id in m1.nodes:
-            sub_coordinates.append(m1.nodes[id]["coords"])
-        sub_coordinates = np.vstack(sub_coordinates)
 
         # Prepare source graph
         n2 = m2.number_of_nodes()
         adj2 = nx.to_numpy_array(m2) + np.eye(n2)
         H2 = onehot_encoding_node(m2, self.embedding_dim)
-        graph_coordinates = []
-        for id in m2.nodes:
-            graph_coordinates.append(m2.nodes[id]["coords"])
-        graph_coordinates = np.vstack(graph_coordinates)
 
         # Aggregation node encoding
         agg_adj1 = np.zeros((n1 + n2, n1 + n2))
@@ -75,6 +67,23 @@ class BaseDataset(Dataset):
         H2 = np.concatenate([np.zeros((n2, self.embedding_dim)), H2], 1)
         H = np.concatenate([H1, H2], 0)
 
+        #prepare graph and cross_graph
+        src_lst, dst_lst = np.where(agg_adj1==1)
+        graph_pt = dgl.graph((src_lst, dst_lst))
+        src_lst_cross, dst_lst_cross = np.where(agg_adj2==1)
+        graph_pt_cross = dgl.graph((src_lst_cross, dst_lst_cross))
+        graph_pt.ndata['feat'] = torch.from_numpy(H).float()
+        graph_pt_cross.ndata['feat'] = torch.from_numpy(H).float()
+        X_pt = []
+        for id in m1.nodes:
+            X_pt.append(m1.nodes[id]["coords"])
+        for id in m2.nodes:
+            X_pt.append(m2.nodes[id]["coords"])
+        X_pt = np.vstack(X_pt)
+        X_pt = torch.from_numpy(X_pt).float()
+        graph_pt.ndata['coords'] = X_pt
+        graph_pt_cross.ndata['coords'] = X_pt
+
         # node indice for aggregation
         valid = np.zeros((n1 + n2,))
         valid[:n1] = 1
@@ -90,20 +99,14 @@ class BaseDataset(Dataset):
         same_label_matrix = np.zeros_like(agg_adj1)
         same_label_matrix[:n1, n1:] = np.copy(dm_new)
         same_label_matrix[n1:, :n1] = np.copy(np.transpose(dm_new))
-        
-        g1 = dgl.graph(([0, 1], [2, 3]))
-        g2 = dgl.graph(([1], [2]))
-        graph = dgl.batch([g1, g2])
-        cross_graph = dgl.batch([g1, g2])
 
         # iso to class
         Y = 1 if "iso" in key else 0
 
         # if n1+n2 > 300 : return None
         sample = {
-            "graph": graph,
-            "cross_graph" : cross_graph,
-            "H": H,
+            "graph": graph_pt,
+            "cross_graph": graph_pt_cross,
             "Y": Y,
             "V": valid,
             "key": key,
@@ -111,7 +114,7 @@ class BaseDataset(Dataset):
             "same_label": same_label_matrix,
         }
 
-        return sample
+        return graph_pt, graph_pt_cross, H, Y, valid, mapping_matrix, same_label_matrix
 
 
 class UnderSampler(Sampler):
@@ -136,37 +139,28 @@ class UnderSampler(Sampler):
 
 
 def collate_fn(batch):
-    max_natoms = max([len(item["H"]) for item in batch if item is not None])
+    graph, cross_graph, H_lst, Y_lst, valid_lst, mapping_lst, label_lst = map(list, zip(*batch))
+    graph = dgl.batch(graph)
+    cross_graph = dgl.batch(cross_graph)
+    max_natoms = max(len(item) for item in H_lst)
 
-    H = np.zeros((len(batch), max_natoms, batch[0]["H"].shape[-1]))
     M = np.zeros((len(batch), max_natoms, max_natoms))
     S = np.zeros((len(batch), max_natoms, max_natoms))
     Y = np.zeros((len(batch),))
     V = np.zeros((len(batch), max_natoms))
 
-    keys = []
-    graph = []
-    cross_graph = []
 
     for i in range(len(batch)):
-        natom = len(batch[i]["H"])
+        natom = len(H_lst[i])
 
-        H[i, :natom] = batch[i]["H"]
-        M[i, :natom, :natom] = batch[i]["mapping"]
-        S[i, :natom, :natom] = batch[i]["same_label"]
-        Y[i] = batch[i]["Y"]
-        V[i, :natom] = batch[i]["V"]
-        keys.append(batch[i]["key"])
-        graph.append(batch[i]["graph"])
-        cross_graph.append(batch[i]["cross_graph"])
+        M[i, :natom, :natom] = mapping_lst[i]
+        S[i, :natom, :natom] = label_lst[i]
+        Y[i] = Y_lst[i]
+        V[i, :natom] = valid_lst[i]
 
-    H = torch.from_numpy(H).float()
     M = torch.from_numpy(M).float()
     S = torch.from_numpy(S).float()
     Y = torch.from_numpy(Y).float()
     V = torch.from_numpy(V).float()
-    graph = dgl.batch(graph)
-    cross_graph = dgl.batch(cross_graph)
 
-
-    return graph, cross_graph, H, M, S, Y, V, keys
+    return graph, cross_graph, M, S, Y, V
