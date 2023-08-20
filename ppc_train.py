b2bb03d266a7aa1f854de152f183bd535bdd1369
vnlvi2k3 <139733764+vnlvi2k3@users.noreply.github.com>
2023-08-20 13:59:42 +0700

    Update ppc_train.py

--- ppc_train.py
+++ ppc_train.py
@@ -206,8 +206,8 @@ def main(args):
             model.zero_grad()
 
             graph, cross_graph, M, S, Y, V  = sample
-            graph = [dgl.from_networkx(item) for item in graph]
-            cross_graph = [dgl.from_networkx(item) for item in cross_graph]
+            graph = [dgl.from_networkx(item, node_attrs = ['coords']) for item in graph]
+            cross_graph = [dgl.from_networkx(item, node_attrs = ['coords']) for item in cross_graph]
             graph = dgl.batch(graph)
             cross_graph = dgl.batch(cross_graph)
 
