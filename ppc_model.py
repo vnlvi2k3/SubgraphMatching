671c38af5c72961837d92ba04bb282bef554c664
vnlvi2k3 <139733764+vnlvi2k3@users.noreply.github.com>
2023-08-21 18:10:09 +0700

    Update ppc_model.py

--- ppc_model.py
+++ ppc_model.py
@@ -109,13 +109,9 @@ class gnn(torch.nn.Module):
             c_hs = c_hs2 - c_hs1
             c_hs = F.dropout(c_hs, p=self.dropout_rate, training=self.training)
 
-        print("shape1:\n", c_hs.shape)
         c_hs = c_hs * p2.unsqueeze(-1).repeat(1, c_hs.size(-1))
-        print("shape2:\n", c_hs.shape)
         c_hs = sum_var_parts(c_hs, graph.batch_num_nodes())
-        print("shape3:\n", c_hs.shape)
         c_hs = c_hs / batch_sub_numnode.unsqueeze(-1).repeat(1, c_hs.size(-1))
-        print("shape4:\n", c_hs.shape)
 
         #Update coords node's data for graph and cross graph
         graph.ndata["upd_coords"] = X_pt
@@ -149,10 +145,13 @@ class gnn(torch.nn.Module):
         # fully connected NN
         c_hs = self.fully_connected(c_hs)
         c_hs = c_hs.view(-1)
+        attn_loss = self.cal_attn_loss(self.cal_atten_batch2(attention, n1, n), attn_masking)
+        rmsd_loss = self.cal_rmsd_loss(c_hs, graph, self.cal_atten_batch1(n1, n, attention), n1, n)
+        pairdst_loss = self.cal_pairdst_loss(graph)
 
         # note that if you don't use concrete dropout, regularization 1-2 is zero
         if training:
-            return c_hs, self.cal_attn_loss(self.cal_atten_batch2(attention, n1, n), attn_masking), self.cal_rmsd_loss(c_hs, graph, self.cal_atten_batch1(n1, n, attention), n1, n), self.cal_pairdst_loss(graph)
+            return c_hs, attn_loss, rmsd_loss, pairdst_loss 
         else:
             return c_hs
 
