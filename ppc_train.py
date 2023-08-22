ff6f40941bd864fff3b9e99e38bcfb469ef22e25
vnlvi2k3 <139733764+vnlvi2k3@users.noreply.github.com>
2023-08-22 14:14:13 +0700

    Update ppc_train.py

--- ppc_train.py
+++ ppc_train.py
@@ -227,7 +227,7 @@ def main(args):
                 X=(graph, cross_graph, V, p2), attn_masking=(M, S), training=True
             )
                         
-            loss =  attn_loss + rmsd_loss
+            loss = loss_fn(pred, Y) + attn_loss + rmsd_loss
             loss.backward()
             optimizer.step()
 
