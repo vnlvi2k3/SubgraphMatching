c6f5606e00377bdf9525d3f9dc5e5217f5ee12c3
vnlvi2k3 <139733764+vnlvi2k3@users.noreply.github.com>
2023-08-22 18:42:35 +0700

    Update ppc_model.py

--- ppc_model.py
+++ ppc_model.py
@@ -179,8 +179,8 @@ class gnn(torch.nn.Module):
         batch_lst = dgl.unbatch(batch_graph)
         batch_rmsd_loss = torch.zeros([]).to(self.device)  
         PP, QQ = self.get_coords(batch_graph, n1)
-        mapping = F.gumbel_softmax(attention, tau=1, hard=True)
-        QQ = torch.mm(mapping, QQ)
+        # mapping = F.gumbel_softmax(attention, tau=1, hard=True)
+        # QQ = torch.mm(mapping, QQ)
         for i in range(len(a)-1):
             P = PP[a[i]:a[i+1],:]
             Q = QQ[a[i]:a[i+1],:]
@@ -198,7 +198,7 @@ class gnn(torch.nn.Module):
             rmsd = rmsd*prob[i]
             batch_rmsd_loss = batch_rmsd_loss + rmsd
         batch_rmsd_loss = batch_rmsd_loss / float(len(batch_lst))
-        print("rmsd:", batch_rmsd_loss)
+        # print("rmsd:", batch_rmsd_loss)
         return batch_rmsd_loss
 
     def get_coords(self, batch_graph, n1):
